55bca69d0a14eaea395cba5a797e7ac5f2ab4b3e
bhaviklotia23 <bhaviklotia22@gmail.com>
2024-01-16 14:51:10 +0530

    Socket Integration finithsed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,21 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useMemo, useState } from "react";
 import { io } from "socket.io-client";
-import { Button, Container, TextField, Typography } from "@mui/material";
+import { Button, Container, Stack, TextField, Typography } from "@mui/material";
 
 const App = () => {
   const socket = useMemo(() => io("http://localhost:5000"), []);
 
   const [message, setMessage] = useState("");
+  const [chats, setChats] = useState([]);
+  const [room, setRoom] = useState("");
+  const [roomName, setRoomName] = useState("");
+  const [socketId, setSocketId] = useState("");
+  console.log(chats, "chats");
 
   useEffect(() => {
     socket.on("connect", () => {
+      setSocketId(socket.id);
       console.log("connected", socket.id);
     });
     socket.on("welcome", (s) => {
@@ -18,6 +24,7 @@ const App = () => {
 
     socket.on("received-message", (data) => {
       console.log(data, "data");
+      setChats((messages) => [...messages, data]);
     });
     return () => {
       socket.disconnect();
@@ -26,27 +33,60 @@ const App = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    socket.emit("message", message);
+    socket.emit("message", { message, room });
     setMessage("");
+  };
+
+  const joinRoomHandler = (e) => {
+    e.preventDefault();
+    socket.emit("join-room", roomName);
+    setRoomName("");
   };
 
   return (
     <Container maxWidth="sm">
-      <Typography variant="h1" component="div" gutterBottom>
-        Welcome to Socket IO
+      <Typography variant="h6" component="div" gutterBottom>
+        {socketId}
       </Typography>
+      <form onSubmit={joinRoomHandler}>
+        <h5>Join Room</h5>
+        <TextField
+          value={roomName}
+          onChange={(e) => setRoomName(e.target.value)}
+          id="outlined-basic"
+          label="Room Name"
+          variant="outlined"
+        />
+        <Button type="submit" variant="contained" color="primary">
+          Join
+        </Button>
+      </form>
       <form onSubmit={handleSubmit}>
         <TextField
           value={message}
           onChange={(e) => setMessage(e.target.value)}
           id="outlined-basic"
-          label="Outlined"
+          label="Message"
+          variant="outlined"
+        />
+        <TextField
+          value={room}
+          onChange={(e) => setRoom(e.target.value)}
+          id="outlined-basic"
+          label="Room"
           variant="outlined"
         />
         <Button type="submit" variant="contained" color="primary">
           Send
         </Button>
       </form>
+      <Stack>
+        {chats.map((chat, index) => (
+          <Typography key={index} variant="h6" component="div" gutterBottom>
+            {chat}
+          </Typography>
+        ))}
+      </Stack>
     </Container>
   );
 };
--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import { Server } from "socket.io";
 import { createServer } from "http";
 import cors from "cors";
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -25,16 +27,55 @@ const io = new Server(server, {
   },
 });
 
+const secretKey = "fgdhfdsvhjv";
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.get("/login", (req, res) => {
+  const token = jwt.sign({ _id: "fgdhfdsvhjv" }, secretKey);
+
+  res
+    .cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    })
+    .json({ message: "Login Success" });
+});
+
+// io.use((socket, next) => {
+//   cookieParser()(socket.request, socket.request.res, (err) => {
+//     if (err) {
+//       return next(err);
+//     }
+
+//     const token = socket.request.cookies.token;
+
+//     if (!token) {
+//       return next(new Error("Auth Error"));
+//     }
+
+//     const decoded = jwt.verify(token, secretKey);
+
+//     next();
+//   });
+// });
 
 io.on("connection", (socket) => {
   console.log("User connected", socket.id);
 
   socket.on("message", (data) => {
     console.log(data);
-    io.emit("received-message", data);
+    // io.emit("received-message", data.message) // Send message to all users including us
+    // socket.broadcast.emit("received-message", data.message) // Send message to all users except us
+    io.to(data.room).emit("received-message", data.message); // Send message to those users who are in same room
+  });
+
+  socket.on("join-room", (room) => {
+    socket.join(room);
+    console.log(`User joined room ${room}`);
   });
 
   socket.on("disconnect", () => {
